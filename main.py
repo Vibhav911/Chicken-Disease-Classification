42eeed8aa2390d58c09aad626f5f1a19944fc0a0
Vibhav Ucharia <vibhav09112003@gmail.com>
2024-05-24 15:14:47 +0530

    Prepare Base Model completed

--- main.py
+++ main.py
@@ -1,15 +1,27 @@
 # This program contains all the pipelines
 from ChickenDisease import logger
 from ChickenDisease.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from ChickenDisease.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 
 
 # Data Ingestion
 STAGE_NAME = 'Data Ingestion Stage'
 try:
     logger.info(f'>>>>>> stage {STAGE_NAME} started <<<<<<<')
-    obj = DataIngestionTrainingPipeline()
-    obj.main()
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
     logger.info(f'>>>>>>> stage {STAGE_NAME} completed <<<<<<<<\n\n x========x')
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+# Prepare Base Model
+STAGE_NAME = "Prepare Base Model"
+try:
+    logger.info(f">>>>>> {STAGE_NAME} Started <<<<<<<<")
+    prepare_base_model = PrepareBaseModelTrainingPipeline()
+    prepare_base_model.main()
+    logger.info(f">>>>>>>> {STAGE_NAME} Completed <<<<<<<<\n\n x=========x")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/ChickenDisease/config/configuration.py
+++ src/ChickenDisease/config/configuration.py
@@ -1,6 +1,7 @@
 from ChickenDisease.constants import *
 from ChickenDisease.utils.common import read_yaml, create_directories
-from ChickenDisease.entity.config_entity import (DataIngestionConfig)
+from ChickenDisease.entity.config_entity import (DataIngestionConfig,
+                                                 PrepareBaseModelConfig)
 
 
 # Configuration Manager
@@ -17,7 +18,7 @@ class ConfigurationManager:
         
       
     # Data Ingestion  
-    def get_data_ingestion_config(self):
+    def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
         
         create_directories([config.root_dir])
@@ -30,3 +31,21 @@ class ConfigurationManager:
         )
         return data_ingestion_config
         
+    
+    # Prepare Base Model
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+        
+        prepare_base_model_config = PrepareBaseModelConfig(
+            root_dir = Path(config.root_dir),
+            base_model_path = Path(config.base_model_path),
+            updated_base_model_path = Path(config.updated_base_model_path),
+            params_image_size = self.params.IMAGE_SIZE,
+            params_learning_rate = self.params.LEARNING_RATE,
+            params_include_top = self.params.INCLUDE_TOP,
+            params_weights = self.params.WEIGHTS,
+            params_classes = self.params.CLASSES
+        )
+        return prepare_base_model_config
